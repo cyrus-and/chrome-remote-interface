1975b5f7548565df0bf81a888f7fcde645e7d5de
Andrey Sidorov <sidorares@yandex.ru>
2013-09-23 15:00:47 +1000

    refactor code to allow changing tab connections

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = function (options, callback) {
     options.host = options.host || 'localhost';
     options.port = options.port || 9222;
     options.chooseTab = options.chooseTab || function () { return 0; };
+  
     var notifier = new events.EventEmitter();
     if (typeof callback === 'function') {
         notifier.on('connect', callback);
@@ -23,7 +24,34 @@ module.exports = function (options, callback) {
                 chrome.close();
             });
         }
-        new Chrome(options, notifier);
+        var chrome = new Chrome(options, notifier);
+        chrome.listTabs(options.host, options.port, function(err, tabs) {
+            if (err)
+               return self.notifier.emit('error', err);
+
+            var tab = tabs[options.chooseTab(tabs)];
+            if (tab) {
+                var tabDebuggerUrl = tab.webSocketDebuggerUrl;
+                if (tabDebuggerUrl) {
+                    chrome.connectToWebSocket.call(self, tabDebuggerUrl);
+                } else {
+                    // a WebSocket is already connected to this tab?
+                    error = new Error('Unable to connect to the WebSocket');
+                    notifier.emit('error', error);
+                }
+            } else {
+                error = new Error('Invalid tab index');
+                notifier.emit('error', error);
+            }
+        });
     });
     return notifier;
+};
+
+module.exports.createClient = function () {
+    var notifier = new events.EventEmitter();
+    var chrome = new Chrome({}, notifier);
+    notifier.on('connect', function() { chrome.emit('connect') });
+    notifier.on('error', function(e) { chrome.emit('error', e) });
+    return chrome;
 };
