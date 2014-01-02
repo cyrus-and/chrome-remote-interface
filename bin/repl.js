7ac2dda08f39e126eb02c9ba5640668f3fa2718c
Andrea Cardaci <cyrus.and@gmail.com>
2014-01-03 00:54:17 +0100

    Allow non-default host and port for the REPL interface

--- bin/repl.js
+++ bin/repl.js
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
 
 var repl = require('repl');
+var program = require('commander');
 var protocol = require('../lib/protocol.json');
 var Chrome = require('../');
 
-Chrome(function (chrome) {
+program
+    .option('-h, --host <host>', 'Remote Debugging Protocol host', 'localhost')
+    .option('-p, --port <port>', 'Remote Debugging Protocol port', '9222')
+    .parse(process.argv)
+
+var options = {
+    'host': program.host,
+    'port': program.port
+};
+
+Chrome(options, function (chrome) {
     var chromeRepl = repl.start({
         'prompt': 'chrome> '
     });
