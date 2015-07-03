de9840e2f075def33333b2a71d3452b20930f45f
Andrea Cardaci <cyrus.and@gmail.com>
2015-07-03 14:19:55 +0200

    Move all the DevTool tests under the same category

--- test/devtools.js
+++ test/devtools.js
@@ -2,7 +2,28 @@ var Chrome = require('../');
 var assert = require('assert');
 var util = require('util');
 
-describe('tabs', function () {
+describe('devtool interaction', function () {
+
+    describe('Protocol', function () {
+        it('should return the protocol description (possibly) from Chrome', function (done) {
+            Chrome.Protocol(function (err, fromChrome, protocol) {
+                assert.ifError(err);
+                assert.equal(typeof protocol, 'object');
+                assert.equal(typeof protocol.version, 'object');
+                done();
+            });
+        });
+        it('should return the hardcoded protocol description', function (done) {
+            Chrome.Protocol({'port':1}, function (err, fromChrome, protocol) {
+                assert.ifError(err);
+                assert(!fromChrome);
+                assert.equal(typeof protocol, 'object');
+                assert.equal(typeof protocol.version, 'object');
+                done();
+            });
+        });
+    });
+
     describe('List',function(){
         it('should return the tab list', function (done) {
             Chrome.List(function (err, tabs) {
@@ -68,4 +89,15 @@ describe('tabs', function () {
             });
         });
     });
+
+    describe('Version',function(){
+        it('should return the version information', function (done) {
+            Chrome.Version(function (err, info) {
+                assert.ifError(err);
+                assert(util.isObject(info));
+                done();
+            });
+        });
+    });
+
 });
