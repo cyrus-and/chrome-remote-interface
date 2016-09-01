d15b42b496933175ce48e883df93fc73d49f6e2c
Andrea Cardaci <cyrus.and@gmail.com>
2016-09-01 23:15:03 +0200

    Test script supports more Node.js versions

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+header() {
+    echo "--- $1 ---"
+}
+
+run-suite() {
+    node_modules/.bin/mocha -b -t 10000
+}
+
 set -e
 
 if ! curl -s 'http://localhost:9222' >/dev/null; then
@@ -7,6 +15,13 @@ if ! curl -s 'http://localhost:9222' >/dev/null; then
     false
 fi
 
+header 'Linter'
 scripts/run-linter.sh
 
-exec node_modules/.bin/mocha -b -t 10000
+header 'Node.js: (default)'
+run-suite
+
+for node_dir; do
+    header "Node.js: '$node_dir'"
+    PATH="$node_dir/bin" run-suite
+done
