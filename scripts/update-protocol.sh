47f0461ace4e786e9e454a5448960f7e60b5a169
Andrea Cardaci <cyrus.and@gmail.com>
2016-10-25 17:08:56 +0200

    Update the 'update-protocol.sh' script

--- scripts/update-protocol.sh
+++ scripts/update-protocol.sh
@@ -6,9 +6,9 @@ browser=$(tempfile)
 js=$(tempfile)
 trap "rm -f '$browser' '$js'" EXIT
 
-base='https://chromium.googlesource.com/chromium/src/+/master/third_party/WebKit/Source'
-curl -s "$base/core/inspector/browser_protocol.json?format=TEXT" | base64 -d >"$browser"
-curl -s "$base/platform/v8_inspector/js_protocol.json?format=TEXT" | base64 -d >"$js"
+base='https://chromium.googlesource.com'
+curl -s "$base/chromium/src/+/master/third_party/WebKit/Source/core/inspector/browser_protocol.json?format=TEXT" | base64 -d >"$browser"
+curl -s "$base/v8/v8.git/+/master/src/inspector/js_protocol.json?format=TEXT" | base64 -d >"$js"
 node -p '
     const protocols = process.argv.slice(1).map((path) => JSON.parse(fs.readFileSync(path)));
     protocols[0].domains.push(...protocols[1].domains);
