cyrus-and/chrome-remote-interface

commit dba3ed81b6db651e184abad10173447c7f024ca2

Rename "tab" to "target" to be more general

bin/client.js
@@ -32,16 +32,16 @@ function inheritProperties(from, to) {
 
 function inspect(target, args, options) {
     options.remote = args.remote;
-    // otherwise the active tab
+    // otherwise the active target
     if (target) {
         if (args.webSocket) {
             // by WebSocket URL
-            options.tab = target;
+            options.target = target;
         } else {
-            // by tab id
-            options.tab = function (tabs) {
-                return tabs.findIndex(function (tab) {
-                    return tab.id === target;
+            // by target id
+            options.target = function (targets) {
+                return targets.findIndex(function (target) {
+                    return target.id === target;
                 });
             };
         }
@@ -186,23 +186,23 @@ function inspect(target, args, options) {
 }
 
 function list(options) {
-    CDP.List(options, function (err, tabs) {
+    CDP.List(options, function (err, targets) {
         if (err) {
             console.error(err.toString());
             process.exit(1);
         }
-        console.log(toJSON(tabs));
+        console.log(toJSON(targets));
     });
 }
 
 function _new(url, options) {
     options.url = url;
-    CDP.New(options, function (err, tab) {
+    CDP.New(options, function (err, target) {
         if (err) {
             console.error(err.toString());
             process.exit(1);
         }
-        console.log(toJSON(tab));
+        console.log(toJSON(target));
     });
 }
 
@@ -259,7 +259,7 @@ program
 program
     .command('inspect [<target>]')
     .description('inspect a target (defaults to the first available target)')
-    .option('-w, --web-socket', 'interpret <target> as a WebSocket URL instead of a tab id')
+    .option('-w, --web-socket', 'interpret <target> as a WebSocket URL instead of a target id')
     .option('-j, --protocol <file.json>', 'Chrome Debugging Protocol descriptor (overrides `--remote`)')
     .option('-r, --remote', 'Attempt to fetch the protocol descriptor remotely')
     .action(function (target, args) {
@@ -268,28 +268,28 @@ program
 
 program
     .command('list')
-    .description('list all the available tabs')
+    .description('list all the available targets/tabs')
     .action(function () {
         action = list;
     });
 
 program
     .command('new [<url>]')
-    .description('create a new tab')
+    .description('create a new target/tab')
     .action(function (url) {
         action = _new.bind(null, url);
     });
 
 program
     .command('activate <id>')
-    .description('activate a tab by id')
+    .description('activate a target/tab by id')
     .action(function (id) {
         action = activate.bind(null, id);
     });
 
 program
     .command('close <id>')
-    .description('close a tab by id')
+    .description('close a target/tab by id')
     .action(function (id) {
         action = close.bind(null, id);
     });

lib/chrome.js
@@ -14,12 +14,12 @@ class Chrome extends EventEmitter {
     constructor(options, notifier) {
         super();
         // options
-        const defaultTab = function (tabs) {
-            const tab = tabs.find((tab) => !!tab.webSocketDebuggerUrl);
-            if (tab) {
-                return tab;
+        const defaultTarget = function (targets) {
+            const target = targets.find((target) => !!target.webSocketDebuggerUrl);
+            if (target) {
+                return target;
             } else {
-                throw new Error ('No inspectable tabs');
+                throw new Error ('No inspectable targets');
             }
         };
         options = options || {};
@@ -28,7 +28,9 @@ class Chrome extends EventEmitter {
         this.secure = !!(options.secure);
         this.protocol = options.protocol;
         this.remote = !!(options.remote);
-        this.tab = options.tab || options.chooseTab || defaultChooseTab;
+        this.target = options.target ||
+            /* backward compatibility */ options.tab || options.chooseTab
+            || defaultTarget;
         // locals
         EventEmitter.call(this);
         this._notifier = notifier;
@@ -140,50 +142,50 @@ function fetchProtocol(options) {
     });
 }
 
-// fetch the WebSocket URL according to 'tab'
+// fetch the WebSocket URL according to 'target'
 function fetchDebuggerURL(options) {
     const chrome = this;
     return new Promise(function (fulfill, reject) {
         // note: when DevTools are open or another WebSocket is connected to a
-        // given tab the 'webSocketDebuggerUrl' field is not available
-        const invalidTabError = function (tab) {
-            return new Error('Invalid tab ' + JSON.stringify(tab, null, 4));
+        // given target the 'webSocketDebuggerUrl' field is not available
+        const invalidTargetError = function (target) {
+            return new Error('Invalid target ' + JSON.stringify(target, null, 4));
         };
         let url;
-        switch (typeof chrome.tab) {
+        switch (typeof chrome.target) {
         case 'string':
             // a WebSocket URL is specified by the user (e.g., node-inspector)
-            fulfill(chrome.tab);
+            fulfill(chrome.target);
             break;
         case 'object':
-            // a tab object is specified by the user
-            url = chrome.tab.webSocketDebuggerUrl;
+            // a target object is specified by the user
+            url = chrome.target.webSocketDebuggerUrl;
             if (url) {
                 fulfill(url);
             } else {
-                reject(invalidTabError(chrome.tab));
+                reject(invalidTargetError(chrome.target));
             }
             break;
         case 'function':
             // a function is specified by the user
-            devtools.List(options).then(function (tabs) {
-                const result = chrome.tab(tabs);
+            devtools.List(options).then(function (targets) {
+                const result = chrome.target(targets);
                 if (typeof result === 'number') {
-                    return tabs[result];
+                    return targets[result];
                 } else {
                     return result;
                 }
-            }).then(function (tab) {
-                url = (tab || {}).webSocketDebuggerUrl;
+            }).then(function (target) {
+                url = (target || {}).webSocketDebuggerUrl;
                 if (url) {
                     fulfill(url);
                 } else {
-                    reject(invalidTabError(tab));
+                    reject(invalidTargetError(target));
                 }
             }).catch(reject);
             break;
         default:
-            reject(new Error('Invalid tab argument "' + chrome.tab + '"'));
+            reject(new Error('Invalid target argument "' + chrome.target + '"'));
         }
     });
 }

test/connect.js
@@ -23,15 +23,15 @@ describe('connecting to Chrome', function () {
                     assert(false);
                 });
             });
-            it('should succeed with custom tab by index', function (done) {
-                Chrome({'tab': function () { return 0; }}, function (chrome) {
+            it('should succeed with custom target by index', function (done) {
+                Chrome({'target': function () { return 0; }}, function (chrome) {
                     chrome.close(done);
                 }).on('error', function () {
                     assert(false);
                 });
             });
-            it('should succeed with custom tab by object', function (done) {
-                Chrome({'tab': function (tabs) { return tabs[0]; }}, function (chrome) {
+            it('should succeed with custom target by object', function (done) {
+                Chrome({'target': function (targets) { return targets[0]; }}, function (chrome) {
                     chrome.close(done);
                 }).on('error', function () {
                     assert(false);
@@ -55,8 +55,8 @@ describe('connecting to Chrome', function () {
                     done();
                 });
             });
-            it('should fail (wrong tab)', function (done) {
-                Chrome({'tab': function () { return -1; }}, function () {
+            it('should fail (wrong target)', function (done) {
+                Chrome({'target': function () { return -1; }}, function () {
                     assert(false);
                 }).on('error', function (err) {
                     assert(err instanceof Error);
@@ -67,7 +67,7 @@ describe('connecting to Chrome', function () {
         describe('two times', function () {
             it('should fail', function (done) {
                 const options = {
-                    chooseTab: () => 0
+                    target: () => 0
                 };
                 Chrome(options, function (chrome) {
                     Chrome(options, function () {
@@ -100,15 +100,15 @@ describe('connecting to Chrome', function () {
                     assert(false);
                 });
             });
-            it('should succeed with custom tab by index', function (done) {
-                Chrome({'tab': function () { return 0; }}).then(function (chrome) {
+            it('should succeed with custom target by index', function (done) {
+                Chrome({'target': function () { return 0; }}).then(function (chrome) {
                     chrome.close(done);
                 }).catch(function () {
                     assert(false);
                 });
             });
-            it('should succeed with custom tab by index', function (done) {
-                Chrome({'tab': function (tabs) { return tabs[0]; }}).then(function (chrome) {
+            it('should succeed with custom target by index', function (done) {
+                Chrome({'target': function (targets) { return targets[0]; }}).then(function (chrome) {
                     chrome.close(done);
                 }).catch(function () {
                     assert(false);
@@ -132,8 +132,8 @@ describe('connecting to Chrome', function () {
                     done();
                 });
             });
-            it('should fail (wrong tab)', function (done) {
-                Chrome({'tab': function () { return -1; }}).then(function () {
+            it('should fail (wrong target)', function (done) {
+                Chrome({'target': function () { return -1; }}).then(function () {
                     done(new Error());
                 }).catch(function (err) {
                     assert(err instanceof Error);
@@ -144,7 +144,7 @@ describe('connecting to Chrome', function () {
         describe('two times', function () {
             it('should fail', function (done) {
                 const options = {
-                    chooseTab: () => 0
+                    target: () => 0
                 };
                 Chrome(options, function (chrome) {
                     Chrome(options).then(function () {

test/devtools.js
@@ -74,19 +74,19 @@ describe('devtool interaction', function () {
     });
     describe('List', function () {
         describe('with callback', function () {
-            it('should return the tab list', function (done) {
-                Chrome.List(function (err, tabs) {
+            it('should return the target list', function (done) {
+                Chrome.List(function (err, targets) {
                     assert.ifError(err);
-                    assert(util.isArray(tabs));
+                    assert(util.isArray(targets));
                     done();
                 });
             });
         });
         describe('without callback', function () {
-            it('should return the tab list', function (done) {
-                Chrome.List().then(function (tabs) {
+            it('should return the target list', function (done) {
+                Chrome.List().then(function (targets) {
                     try {
-                        assert(util.isArray(tabs));
+                        assert(util.isArray(targets));
                         done();
                     } catch (err) {
                         done(err);
@@ -99,45 +99,45 @@ describe('devtool interaction', function () {
     });
     describe('New', function () {
         describe('with callback', function () {
-            it('should spawn a tab to a specific URL', function (done) {
-                Chrome.New({url: 'chrome://newtab/'}, function (err, tab) {
+            it('should spawn a target to a specific URL', function (done) {
+                Chrome.New({url: 'chrome://newtab/'}, function (err, target) {
                     assert.ifError(err);
-                    assert(tab.id);
-                    Chrome.List(function (err, tabs) {
-                        assert(tabs.some(function (t) {
-                            return t.id === tab.id;
+                    assert(target.id);
+                    Chrome.List(function (err, targets) {
+                        assert(targets.some(function (t) {
+                            return t.id === target.id;
                         }));
-                        assert(util.isArray(tabs));
-                        assert.equal(tab.url, 'chrome://newtab/');
+                        assert(util.isArray(targets));
+                        assert.equal(target.url, 'chrome://newtab/');
                         done();
                     });
                 });
             });
-            it('should spawn a new tab', function (done) {
-                Chrome.New(function (err, tab) {
+            it('should spawn a new target', function (done) {
+                Chrome.New(function (err, target) {
                     assert.ifError(err);
-                    assert(tab.id);
-                    Chrome.List(function (err, tabs) {
-                        assert(tabs.some(function (t) {
-                            return t.id === tab.id;
+                    assert(target.id);
+                    Chrome.List(function (err, targets) {
+                        assert(targets.some(function (t) {
+                            return t.id === target.id;
                         }));
-                        assert(util.isArray(tabs));
+                        assert(util.isArray(targets));
                         done();
                     });
                 });
             });
         });
         describe('without callback', function () {
-            it('should spawn a tab to a specific URL', function (done) {
-                Chrome.New({url: 'chrome://newtab/'}).then(function (tab) {
+            it('should spawn a target to a specific URL', function (done) {
+                Chrome.New({url: 'chrome://newtab/'}).then(function (target) {
                     try {
-                        assert(tab.id);
-                        Chrome.List(function (err, tabs) {
-                            assert(tabs.some(function (t) {
-                                return t.id === tab.id;
+                        assert(target.id);
+                        Chrome.List(function (err, targets) {
+                            assert(targets.some(function (t) {
+                                return t.id === target.id;
                             }));
-                            assert(util.isArray(tabs));
-                            assert.equal(tab.url, 'chrome://newtab/');
+                            assert(util.isArray(targets));
+                            assert.equal(target.url, 'chrome://newtab/');
                             done();
                         });
                     } catch (err) {
@@ -147,15 +147,15 @@ describe('devtool interaction', function () {
                     assert(false);
                 });
             });
-            it('should spawn a new tab', function (done) {
-                Chrome.New().then(function (tab) {
+            it('should spawn a new target', function (done) {
+                Chrome.New().then(function (target) {
                     try {
-                        assert(tab.id);
-                        Chrome.List(function (err, tabs) {
-                            assert(tabs.some(function (t) {
-                                return t.id === tab.id;
+                        assert(target.id);
+                        Chrome.List(function (err, targets) {
+                            assert(targets.some(function (t) {
+                                return t.id === target.id;
                             }));
-                            assert(util.isArray(tabs));
+                            assert(util.isArray(targets));
                             done();
                         });
                     } catch (err) {
@@ -169,11 +169,11 @@ describe('devtool interaction', function () {
     });
     describe('Activate', function () {
         describe('with callback', function () {
-            it('should activate an existing tab', function (done) {
-                Chrome.List(function (err, tabs) {
-                    // tabs[0] is the latest tab to be spawned
-                    const tab = tabs[0];
-                    Chrome.Activate({id: tab.id}, function (err) {
+            it('should activate an existing target', function (done) {
+                Chrome.List(function (err, targets) {
+                    // targets[0] is the latest target to be spawned
+                    const target = targets[0];
+                    Chrome.Activate({id: target.id}, function (err) {
                         assert.ifError(err);
                         done();
                     });
@@ -181,11 +181,11 @@ describe('devtool interaction', function () {
             });
         });
         describe('without callback', function () {
-            it('should activate an existing tab', function (done) {
-                Chrome.List(function (err, tabs) {
-                    // tabs[0] is the latest tab to be spawned
-                    const tab = tabs[0];
-                    Chrome.Activate({id: tab.id}).then(function () {
+            it('should activate an existing target', function (done) {
+                Chrome.List(function (err, targets) {
+                    // targets[0] is the latest target to be spawned
+                    const target = targets[0];
+                    Chrome.Activate({id: target.id}).then(function () {
                         done();
                     }).catch(function () {
                         assert(false);
@@ -196,13 +196,13 @@ describe('devtool interaction', function () {
     });
     describe('Close', function () {
         describe('with callback', function () {
-            it('should close an existing tab', function (done) {
-                Chrome.List(function (err, tabs) {
-                    // tabs[0] is the latest tab to be spawned
-                    const tab = tabs[0];
-                    Chrome.Close({id: tab.id}, function (err) {
+            it('should close an existing target', function (done) {
+                Chrome.List(function (err, targets) {
+                    // targets[0] is the latest target to be spawned
+                    const target = targets[0];
+                    Chrome.Close({id: target.id}, function (err) {
                         assert.ifError(err);
-                        // avoid that further test cases attach to this tab as the
+                        // avoid that further test cases attach to this target as the
                         // actual close is a bit delayed
                         setTimeout(done, 1000);
                     });
@@ -210,12 +210,12 @@ describe('devtool interaction', function () {
             });
         });
         describe('without callback', function () {
-            it('should close an existing tab', function (done) {
-                Chrome.List(function (err, tabs) {
-                    // tabs[0] is the latest tab to be spawned
-                    const tab = tabs[0];
-                    Chrome.Close({id: tab.id}).then(function () {
-                        // avoid that further test cases attach to this tab as the
+            it('should close an existing target', function (done) {
+                Chrome.List(function (err, targets) {
+                    // targets[0] is the latest target to be spawned
+                    const target = targets[0];
+                    Chrome.Close({id: target.id}).then(function () {
+                        // avoid that further test cases attach to this target as the
                         // actual close is a bit delayed
                         setTimeout(done, 1000);
                     }).catch(function () {

commit a62257cb684c2f443874f92c54ba916f0b879acc

Fix inspect by id in CLI
Bug introduced in dba3ed81b6db651e184abad10173447c7f024ca2

bin/client.js
@@ -41,8 +41,8 @@ function inspect(target, args, options) {
         } else {
             // by target id
             options.target = function (targets) {
-                return targets.findIndex(function (target) {
-                    return target.id === target;
+                return targets.findIndex(function (_target) {
+                    return _target.id === target;
                 });
             };
         }