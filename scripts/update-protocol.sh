e66b62238ff10833e248a1cc069a337eecd7091c
Andrea Cardaci <cyrus.and@gmail.com>
2018-03-13 15:05:30 +0100

    Make the protocol update script more portable

--- scripts/update-protocol.sh
+++ scripts/update-protocol.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-browser=$(tempfile)
-js=$(tempfile)
+browser=$(mktemp)
+js=$(mktemp)
 trap "rm -f '$browser' '$js'" EXIT
 
 base='https://chromium.googlesource.com'
-curl -s "$base/chromium/src/+/master/third_party/WebKit/Source/core/inspector/browser_protocol.json?format=TEXT" | base64 -d >"$browser"
-curl -s "$base/v8/v8/+/master/src/inspector/js_protocol.json?format=TEXT" | base64 -d >"$js"
+curl -s "$base/chromium/src/+/master/third_party/WebKit/Source/core/inspector/browser_protocol.json?format=TEXT" | python -m base64 -d >"$browser"
+curl -s "$base/v8/v8/+/master/src/inspector/js_protocol.json?format=TEXT" | python -m base64 -d >"$js"
 node -p '
     const protocols = process.argv.slice(1).map((path) => JSON.parse(fs.readFileSync(path)));
     protocols[0].domains.push(...protocols[1].domains);
