cyrus-and/chrome-remote-interface

commit 0b4ffa4645cc9c7bc25826b412ffd652353f51eb

Coherently avoid '.js' extension in requires

index.js
@@ -2,8 +2,8 @@
 
 const EventEmitter = require('events');
 
-const devtools = require('./lib/devtools.js');
-const Chrome = require('./lib/chrome.js');
+const devtools = require('./lib/devtools');
+const Chrome = require('./lib/chrome');
 
 module.exports = function (options, callback) {
     if (typeof options === 'function') {

lib/chrome.js
@@ -5,9 +5,9 @@ const util = require('util');
 
 const WebSocket = require('ws');
 
-const api = require('./api.js');
-const defaults = require('./defaults.js');
-const devtools = require('./devtools.js');
+const api = require('./api');
+const defaults = require('./defaults');
+const devtools = require('./devtools');
 const ProtocolError = require('./protocol-error');
 
 class Chrome extends EventEmitter {

lib/devtools.js
@@ -3,8 +3,8 @@
 const http = require('http');
 const https = require('https');
 
-const defaults = require('./defaults.js');
-const externalRequest = require('./external-request.js');
+const defaults = require('./defaults');
+const externalRequest = require('./external-request');
 
 // callback(err, protocol)
 module.exports.Protocol = promisesWrapper(function (options, callback) {

commit 85b1fac9c42e7db2074e73ec5de168ea9bd929c5

Make the require extensions explicit to avoid problems with webpack
This bug was firstly introduced in 0b4ffa4645cc9c7bc25826b412ffd652353f51eb.

Fixes #346.

bin/client.js
@@ -10,7 +10,7 @@ const path = require('path');
 const program = require('commander');
 
 const CDP = require('../');
-const packageInfo = require('../package');
+const packageInfo = require('../package.json');
 
 function display(object) {
     return util.inspect(object, {

index.js
@@ -2,8 +2,8 @@
 
 const EventEmitter = require('events');
 
-const devtools = require('./lib/devtools');
-const Chrome = require('./lib/chrome');
+const devtools = require('./lib/devtools.js');
+const Chrome = require('./lib/chrome.js');
 
 module.exports = function (options, callback) {
     if (typeof options === 'function') {

lib/chrome.js
@@ -6,9 +6,9 @@ const parseUrl = require('url').parse;
 
 const WebSocket = require('ws');
 
-const api = require('./api');
-const defaults = require('./defaults');
-const devtools = require('./devtools');
+const api = require('./api.js');
+const defaults = require('./defaults.js');
+const devtools = require('./devtools.js');
 
 class ProtocolError extends Error {
     constructor(request, response) {

lib/devtools.js
@@ -3,8 +3,8 @@
 const http = require('http');
 const https = require('https');
 
-const defaults = require('./defaults');
-const externalRequest = require('./external-request');
+const defaults = require('./defaults.js');
+const externalRequest = require('./external-request.js');
 
 // callback(err, protocol)
 module.exports.Protocol = promisesWrapper(function (options, callback) {